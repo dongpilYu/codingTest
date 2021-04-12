5f309760b5a1509732865794e207e6621fc1a726
dongpil_yu <yoodongphil@naver.com>
2021-04-12 21:27:00 +0900

    카카오 순위검색2

--- src/카카오_순위검색_2.java
+++ src/카카오_순위검색_2.java
@@ -1,43 +1,79 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+/*
+    2021 kakao blind test - 순위 검색
+    https://programmers.co.kr/learn/courses/30/lessons/72412
+ */
 import java.util.ArrayList;
 import java.util.Collections;
 
-class kakao_2021 {
+class 카카오_순위검색_2 {
     public static void main(String[] args){
         String[] info = {"java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"};
         String[] query = {"java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"};
 
-        int[] an = kakao_2021.solution(info, query);
+        int[] an = 카카오_순위검색_2.solution(info, query);
         for(int i=0;i<an.length;i++)
             System.out.print(an[i] + " ");
     }
     public static int[] solution(String[] info, String[] query) {
-        int[] answer = new int[query.length];
-         /*
+        /*
             모든 경우를 상수화 (108 case)
-            query에 해당하는 케이스를 하나의 배열에서 찾기 위해 -를 고려해서 만
+            query에 해당하는 케이스를 하나의 배열에서 찾기 위해 -를 고려해서 만듦
+            4차원으로 표현할 수 있는 걸 1차원으로 표현
+            4차원으로 표현하면 아래와 같다.
+            ArrayList<Integer>[][][][][] = new ArrayList<Integer>[3][2][2][2];
          */
+        int[] answer = new int[query.length];
         int lang_none = 0, cpp = 27, java = 54, python = 81;
         int pos_none = 0, backend = 9, frontend = 18;
-        int tmp_none = 0, junior= 3, senior = 6;
+        int tmp_none = 0, junior = 3, senior = 6;
         int food_none = 0, chicken = 1, pizza = 2;
 
+        // 1차원으로 할 경우
         ArrayList<ArrayList<Integer>> data = new ArrayList<ArrayList<Integer>>();
-        for (int i=0;i<108;i++){
-            ArrayList<Integer> data0 =  new ArrayList<Integer>();
+        for (int i = 0; i < 108; i++) {
+            ArrayList<Integer> data0 = new ArrayList<Integer>();
             data.add(data0);
         }
+        /*
+        // 만약 1차원으로 표현안하고, 4차원으로 했다면 아래와 같이 해야 한다.
+        ArrayList<Integer>[][][][] data2 = new ArrayList[4][3][3][3];
+
+        // 초기화 및 출력 예시
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    for (int l = 0; l < 3; l++) {
+                        data2[i][j][k][l] = new ArrayList<Integer>();
+                        int number = (int) (Math.random() * 4);
+                        for (int q = 0; q < number; q++) {
+                            data2[i][j][k][l].add((int) (Math.random()));
+                        }
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                for(int k=0;k<3;k++){
+                    for(int l=0;l<3;l++){
+                        System.out.printf("%d ", data2[i][j][k][l].size());
+                    }
+                    System.out.printf("\n");
+                }
+                System.out.printf("\n");
+            }
+            System.out.printf("\n");
+        }
+        */
 
          /*
 
-         1. 맵 생성 : length
-         2. 정렬 : n log n
-         3. 탐색 : query
+         1. 맵 생성 : info length
+         2. 정렬 : n log n (n은 각 인덱스에 할당된 리스트의 크기)
+         3. 탐색 : query length
 
          */
-        int num =0;
+        int num = 0;
         int[] num_list = {0,0,0,0};
         for(int i=0;i<info.length;i++){
             //System.out.println(info[i]);
