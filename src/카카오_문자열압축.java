21951385828e118af08f0187d6ec8453f9bae70e
dongpil_yu <yoodongphil@naver.com>
2021-04-14 20:13:32 +0900

    카카오 인턴 - 문자열 압축

---
+++ src/카카오_문자열압축.java
@@ -0,0 +1,57 @@
+/*
+        카카오 - 문자열 압축
+       https://programmers.co.kr/learn/courses/30/lessons/60057
+
+        반복문 내부에서 인덱스 건들지 말자
+*/
+public class 카카오_문자열압축 {
+
+    public static void main(String[] args){
+        카카오_문자열압축.solution("aabbbcb");
+    }
+    public static int solution(String s){
+        int answer = 1000;
+
+        for(int i=1;i<=s.length();i++){
+            String str = tmp(s, i);
+            if(str.length() < answer)
+                answer = str.length();
+        }
+        return answer;
+    }
+
+    public static String tmp(String s, int d){
+        String result = "";
+        int idx = 0;
+        // int leap = 1; // point3
+        /*
+        while(s.length() < idx + (a+1)*d){
+        }
+        */
+        for(int a=1;;a++){
+            int first = idx + (a-1)*d;
+            int second = idx + (a)*d;
+            int third = idx + (a+1)*d;
+
+            if(s.length() < third) {
+                 if(a != 1)
+                    result += (Integer.toString(a));
+                result += s.substring(first, second);
+                result += s.substring(second);
+                break;
+            }
+
+            if(s.substring(first, second).compareTo(s.substring(second, third)) != 0){
+                idx += a * d;
+                if(a != 1)
+                    result += (Integer.toString(a));
+                result += s.substring(first, second);
+                a = 0;
+            }
+        }
+
+
+        return result;
+
+    }
+}
