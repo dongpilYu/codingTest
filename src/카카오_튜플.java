2a05ee1f7c1ce5f8920b2c472148495c1d373517
dongpil_yu <dongpil_yu@tmax.co.kr>
2021-04-25 17:50:58 +0900

    프로그래머스 레벌2 > 튜플

--- src/카카오_튜플.java
+++ src/카카오_튜플.java
@@ -1,6 +1,23 @@
 /*
     프로그래머스 레벌2 > 튜플
     https://programmers.co.kr/learn/courses/30/lessons/64065
+    
+    길이가 작은 것 부터 하나씩 set에 넣고, set을 list로 변환
+    
+    다른 사람의 풀이)
+    1. 정규 표현식으로 불필요한 문자 제거
+        - replaceAll, trim, split
+    2. 문자열 길이로 정렬  
+        - Arrays.sort(arr, (a,b) -> {return a.length() > b.length();});
+    3. HashSet 자체가 존재 여부에 따라 true/false를 반환하므로 반환값에 따라 배열에 삽입
+    
+    linkedList는 내부적으로 doubly linked list를 사용해 값을 저장
+    linkedHashSet은 내부적으로 linked hashmap을 써서 값을 저장
+
+    // set은 순서를 유지하지 않고, 중복을 허락하지 않는 자료구조
+    // hashSet : 해시 알고리즘을 사용해 값을 저장
+    // linkedHashSet : 저장 순서를 유지
+    // linkedList vs linkedHashSet
  */
 import java.util.*;
 
@@ -17,12 +34,8 @@ public class 카카오_튜플 {
     public static int[] solution(String s) {
         int[] answer = {};
 
-        String s2 = s.replace("{", "");
-        String s3 = s2.replace("}}", "");
-        String[] strs = s3.split("},");
-        //for(int i=0;i<strs.length;i++)
-        //    System.out.println(strs[i]);
-
+        String[] strs = s.replaceAll("[{]", " ").replaceAll("[}]", " ").trim().split(" , ");
+        
         HashMap<Integer, String[]> hm = new HashMap<>();
         for(int i=0;i<strs.length;i++){
             String[] s4 = strs[i].split(",");
@@ -32,19 +45,21 @@ public class 카카오_튜플 {
         for(Map.Entry<Integer, String[]> entry : hm.entrySet()){
             int key = entry.getKey();
             String[] str = entry.getValue();
-            /*
-            System.out.println("key: " + key);
-            for(int i=0;i<str.length;i++)
-                System.out.print(str[i] + " ");
-            System.out.println();
-            */
         }
 
-        // set은 순서를 유지하지 않고, 중복을 허락하지 않는 자료구조
-        // hashSet : 해시 알고리즘을 사용해 값을 저장
-        // linkedHashSet : 저장 순서를 유지
-        // linkedList vs linkedHashSet
 
+        Set<String> hs2 = new HashSet<>();
+        answer = new int[strs.length];
+        int idx = 0;
+        for(int i=1;i<strs.length+1;i++){
+            String[] str = hm.get(i);
+            for(int j=0;j<str.length;j++){
+                if(hs2.add(str[j])){
+                    answer[idx++] = Integer.parseInt(str[j]);
+                }
+            }
+        }
+        /*
         LinkedHashSet<Integer> hs = new LinkedHashSet<>();
         for(int i=1;i<strs.length+1;i++){
             String[] str = hm.get(i);
@@ -54,12 +69,11 @@ public class 카카오_튜플 {
             }
         }
 
-        answer = new int[hs.size()];
         int idx = 0;
         for(Integer i : hs){
             answer[idx++] = i;
         }
-
+        */
 
         return answer;
     }
